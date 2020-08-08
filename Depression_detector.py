1f3265423170315136742d2ca8b0eca8f9af241f
Unknown <unknown@example.com>
2020-08-08 21:14:52 +0200

    Styling some lines

--- Depression_detector.py
+++ Depression_detector.py
@@ -23,32 +23,34 @@ Dp = preprocessing()
 training_positive_dateframe = pd.concat(Dd.parse_folder(training_positive_path))
 training_negative_dataframe = pd.concat(Dd.parse_folder(training_negative_path))
 test_dataframe = pd.concat(Dd.parse_folder(test_path))
-#
+
 # #add labels to positive and negative subjects training dataset
 training_positive_dateframe['LABEL'] = 1
 training_negative_dataframe['LABEL'] = 0
-#
+
 # #adding label to test dataframe
 test_dataframe = Dd.prepare_test_dataframe(test_dataframe)
-#
+
 # #save them to csv file
 training_positive_dateframe.to_csv('training_positive_dateframe.csv')
 training_negative_dataframe.to_csv('training_negative_dataframe.csv')
 test_dataframe.to_csv('test_dataframe.csv')
-#
+
 
 
 positive_training_file_CSV = 'training_positive_dateframe.csv'
 negative_training_file_CSV = 'training_negative_dataframe.csv'
-#
-#
-#
+
+
+
 unified_training_df = Dd.Unifing_training_data(positive_training_file_CSV,negative_training_file_CSV)
 print(unified_training_df.shape)
 unified_training_df.set_index('ID')
+
 # #Concatentenate title with text
 unified_training_df["TITLE_TEXT"] = unified_training_df["TITLE"] + unified_training_df["TEXT"]
 unified_training_df.to_csv('unified_training_df.csv')
+
 # unified_training_df = pd.read_csv('unified_training_df.csv')
 unified_training_df = unified_training_df.drop(['TITLE', 'INFO', 'TEXT' ], axis=1)
 
